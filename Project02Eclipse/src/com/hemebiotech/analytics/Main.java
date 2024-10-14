38c736bfac95623b253eb0f68e3610e8f3bacf0d
Johan <labbejohan26@gmail.com>
2024-10-14 11:00:19 +0200

    Code Optimisation

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,10 +1,5 @@
-package com.hemebiotech;
+package com.hemebiotech.analytics;
 
-import com.hemebiotech.analytics.AnalyticsCounter;
-import com.hemebiotech.analytics.ISymptomReader;
-import com.hemebiotech.analytics.ISymptomWriter;
-import com.hemebiotech.analytics.ReadSymptomDataFromFile;
-import com.hemebiotech.analytics.WriteSymptomDataToFile;
 import java.util.List;
 import java.util.Map;
 
